781a4e0f90a5128b3d8fed274b0c3d9e85e51c31
rai8 <hytonne@outlook.com>
2020-09-06 16:42:20 +0300

    working with drawer, tiles and LIstviews

--- lib/main.dart
+++ lib/main.dart
@@ -22,37 +22,54 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          padding: const EdgeInsets.all(0),
-          //color: Colors.purple, - we use decoration instead
-          width: 100,
           height: 100,
-          decoration: BoxDecoration(
-            color: Colors.pink,
-            //shape: BoxShape.circle, //wraps the container in a circle like format
-            borderRadius: BorderRadius.circular(8),
-            gradient: LinearGradient(colors: [
-              Colors.pink,
-              Colors.yellow,
-            ]),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 10,
-              )
-            ]
-
-          ),
-          alignment: Alignment.center, //align text at the center of the container/div
-          child: Text("I am a box", textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-          
-          ),),
-        
+          width: 100,
+          color: Colors.teal,
+         
+         
         ),
       ),
-    );
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Rytone Odhimabo"), 
+              accountEmail: Text("hytonne@outlook.com"),
+              /* currentAccountPicture: Image.network(
+                "https://images.unsplash.com/photo-1457052002176-2d16a4f4a3ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                ), */
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1457052002176-2d16a4f4a3ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                ),
+              ),
+              ),
+           /*  DrawerHeader(
+              child: Text('Hi I am drawer'),
+              decoration: BoxDecoration(
+              color: Colors.green[200],
+            ),), */
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Rhytonne Odhimabo"),
+              subtitle: Text('Developer'),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text('hytonne@outlook.com'),
+              trailing: Icon(Icons.edit),
+              onTap: (){},
+            ),
+          ],
+        ),
+      ), // that ka small menu bar on the top left
+      floatingActionButton: FloatingActionButton(onPressed: (){},
+      child: Icon(Icons.edit),
+     // mini: true, //mkes the floating button smaller
+      ),
+    ); 
   }
 }
