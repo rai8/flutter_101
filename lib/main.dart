0e782c15174763b68ab50f60fcb936b5eddf927f
rai8 <hytonne@outlook.com>
2020-09-06 12:03:42 +0300

    text style and container decoration

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,38 @@ class HomePage extends StatelessWidget {
         title: Text("Awesome App"),
         
       ),
-      body: Container(
-        child: Text('Hello fluter'),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(0),
+          //color: Colors.purple, - we use decoration instead
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.pink,
+            //shape: BoxShape.circle, //wraps the container in a circle like format
+            borderRadius: BorderRadius.circular(8),
+            gradient: LinearGradient(colors: [
+              Colors.pink,
+              Colors.yellow,
+            ]),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10,
+              )
+            ]
+
+          ),
+          alignment: Alignment.center, //align text at the center of the container/div
+          child: Text("I am a box", textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          
+          ),),
+        
+        ),
       ),
     );
   }
